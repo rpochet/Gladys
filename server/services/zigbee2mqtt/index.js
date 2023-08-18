a42f96e61df18b2ec36e5006e36efe1f5e623650
POCHET Romuald <pochet.romuald@gmail.com>
2023-08-18 09:33:00 +0200

    Add Zigbee2mqtt status

--- server/services/zigbee2mqtt/index.js
+++ server/services/zigbee2mqtt/index.js
@@ -39,10 +39,25 @@ module.exports = function Zigbee2mqttService(gladys, serviceId) {
     return zigbee2mqttManager.gladysConnected && zigbee2mqttManager.zigbee2mqttConnected;
   }
 
+  /**
+   * @public
+   * @description Get Zigbee2mqtt status.
+   * @returns {Promise<boolean>} Returns Zigbee2mqtt status.
+   * @example
+   *  const used = await gladys.services.zigbee2mqtt.getStatus();
+   */
+  async function getStatus() {
+    return {
+      configured: zigbee2mqttManager.usbConfigured && zigbee2mqttManager.networkModeValid && zigbee2mqttManager.dockerBased,
+      connected: zigbee2mqttManager.gladysConnected && zigbee2mqttManager.zigbee2mqttConnected,
+    };
+  }
+
   return Object.freeze({
     start,
     stop,
     isUsed,
+    getStatus:  mqttHandler.status,
     device: zigbee2mqttManager,
     controllers: Zigbee2mqttController(gladys, zigbee2mqttManager),
   });
