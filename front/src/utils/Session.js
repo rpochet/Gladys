543c5aa085bb1f8513ab70304691977ff9612123
POCHET Romuald <pochet.romuald@gmail.com>
2023-11-29 23:04:32 +0100

    WS

--- front/src/utils/Session.js
+++ front/src/utils/Session.js
@@ -35,7 +35,7 @@ class Session {
   }
 
   connect() {
-    const websocketUrl = window.location.origin.replace('http', 'ws') + '/ws';
+    const websocketUrl = window.location.origin.replace('https', 'ws').replace('http', 'ws') + '/ws';
     if (this.ws && this.ws.close) {
       this.ws.close();
     }
